efda35fac794c2c5d94e5d3aa12557d2b9e699da
AnikHaque <ekramul15-811@diu.edu.bd>
2024-03-21 15:28:36 +0600

    aythprovider added

--- src/index.js
+++ src/index.js
@@ -9,13 +9,16 @@ import {
 import { router } from './Routes/Routes';
 import { Provider } from 'react-redux';
 import { store } from './redux/app/store';
+import AuthProvider from './providers/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router}></RouterProvider>
-    </Provider>
+    <AuthProvider>
+      <Provider store={store}>
+        <RouterProvider router={router}></RouterProvider>
+      </Provider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
