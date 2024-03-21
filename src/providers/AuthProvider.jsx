8483831ed2514420ef499f88f34d5c4d80b40f50
AnikHaque <ekramul15-811@diu.edu.bd>
2024-03-21 15:54:30 +0600

    firebase signIn implemented

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,13 +1,58 @@
-import { Children, createContext, useState } from "react"
+import { Children, createContext, useEffect, useState } from "react"
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+
 
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed up 
+                const user = userCredential.user;
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
+    }
+
+const signIn =(email, password) =>{
+setLoading(true);
+    return signInWithEmailAndPassword(email, password)
+        
+}
+
+const logOut = () => {
+   setLoading(true);
+   return signOut(auth);
+}
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('current', currentUser);
+            setLoading(false);
+
+        });
+        return ()=> {
+            return unsubscribe();
+        }
+    },[])
     const authInfo = {
 user,
-loading
+loading,
+createUser,
+signIn,
+logOut
     }
   return (
    <AuthContext.Provider value={authInfo}>
